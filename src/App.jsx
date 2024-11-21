eeb155a31fcfc7b11b89e487732c91d73897e5c5
Zachary48 <158526110+Zachary48@users.noreply.github.com>
2024-11-20 20:43:43 -0600

    Making it look more professional

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Home from './components/Home';
+
 
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Hello, World!</h1>
-      <p>This is a test to see if the page renders correctly.</p>
+    
+      return <Home />;
     </div>
   );
 }
