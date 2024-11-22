d6d4aa314b22613e882330c8caafed3774db402a
Zachary48 <158526110+Zachary48@users.noreply.github.com>
2024-11-21 22:05:16 -0600

    Massive redesign part 3 of 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import UserPage from './components/UserPage';
 import Auth from './components/Auth';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Home page route */}
         <Route path="/" element={<Home />} />
+
+        {/* User page route */}
+        <Route path="/user" element={<UserPage />} />
+
+        {/* Authentication routes */}
         <Route path="/signin" element={<Auth mode="signin" />} />
         <Route path="/register" element={<Auth mode="register" />} />
       </Routes>
